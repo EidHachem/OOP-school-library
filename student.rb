ec404293e21eb117157dca13ee51c55a18eb9a93
EidHachem <eidhachem1@gmail.com>
2022-07-18 16:31:48 +0300

    Edit flies to meet required changes

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require_relative './person'
 # student class
 class Student < Person
-  def initialize(classroom)
+  def initialize(classroom, age, name = 'Unknown', parent_permission: 'true')
     super(age, name, parent_permission)
     @classroom = classroom
   end
