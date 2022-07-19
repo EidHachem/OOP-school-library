82394f693c6d98150895c5f7b2237df023b16fcb
EidHachem <eidhachem1@gmail.com>
2022-07-19 15:42:53 +0300

    Create nameable and decorator classes

--- person.rb
+++ person.rb
@@ -1,9 +1,12 @@
+require_relative './nameable'
+require_relative './decorator'
 # Person class
-class Person
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission = 'true')
+    super()
     @id = Random.rand(1..10.000)
     @name = name
     @age = age
@@ -14,9 +17,20 @@ class Person
     of_age? || parent_permission
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+
+# person = Person.new(22, 'maximilianus')
+# p  person.correct_name
+#  capitalizedPerson = CapitalizeDecorator.new(person)
+# p  capitalizedPerson.correct_name
+#  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+# p  capitalizedTrimmedPerson.correct_name
