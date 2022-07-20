3f78dd172c11c928539e9fa08ea020d045def8fe
EidHachem <eidhachem1@gmail.com>
2022-07-20 18:42:59 +0300

    Add book, classroom and rental classes and apply requirments

--- person.rb
+++ person.rb
@@ -1,8 +1,9 @@
 require_relative './nameable'
 require_relative './decorator'
+require_relative './rental'
 # Person class
 class Person < Nameable
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission = 'true')
@@ -11,6 +12,11 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, book, self)
   end
 
   def can_use_services?
